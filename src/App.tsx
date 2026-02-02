9a780d20b65cc10a1dc3a7b066ed75b732719bf5
shivam yadav <shivamyadavskn@gmail.com>
2026-02-02 23:02:16 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import { useState } from "react";
-import { useAppDispatch } from "./app/hook";
-import { createPost } from "./features/posts/postsSlice";
-import ParentContainer from "./components/ParentContainer";
+
 
 function App() {
 
-  const dispatch = useAppDispatch();
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-
-
-  const handleCreatePost = () => {
-    if (title && body) {
-      dispatch(createPost({ title, body }));
-      setTitle("");
-      setBody("");
-    }
-  };
-
   return (
     <div>
-      <h2>Create Post</h2>
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Title"
-      />
-      <input
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-        placeholder="Body"
-      />
-      <button onClick={handleCreatePost}>Submit</button>
-
-
-      <ParentContainer />
+      jkkj
     </div>
   );
 }
